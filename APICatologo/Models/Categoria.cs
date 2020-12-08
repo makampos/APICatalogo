e0e30cbf0baaf9677bc4a0d9db097ef2f5bd16b5
makampos <makampos@DESKTOP-EDAK032>
2020-12-08 16:45:32 -0300

    feat: adicionado anotações nas propriedades das entidades

--- APICatologo/Models/Categoria.cs
+++ APICatologo/Models/Categoria.cs
@@ -1,19 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace APICatologo.Models
 {
+    [Table("Categorias")]
     public class Categoria
     {
         public Categoria ()
         {            
             Produtos = new Collection<Produto>();
         }
+        [Key]
         public int CategoriaId { get; set; }
-        public string Name { get; set; }
+        [Required]
+        [MaxLength(80)]
+        public string Nome { get; set; }
+        [Required]
+        [MaxLength(300)]
         public string ImagemUrl { get; set; }
 
         // Propriedade de navegação -> 1:N
