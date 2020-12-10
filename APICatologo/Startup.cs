9cb2ff036577efdc78d4e30e6df2698dc6caac6d
makampos <makampos@DESKTOP-EDAK032>
2020-12-10 18:57:40 -0300

    feat: adicionado referência ao pacote NewtonsoftJson e configurado opção para ignorar exceção cíclica

--- APICatologo/Startup.cs
+++ APICatologo/Startup.cs
@@ -23,7 +23,11 @@ namespace APICatologo
         {
             services.AddDbContext<AppDbContext>(options => options.UseMySql
             (Configuration.GetConnectionString("DefaultConnection")));
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(options => {
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            });
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
