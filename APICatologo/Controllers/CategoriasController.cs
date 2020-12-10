8386742064f61533a7397c408b88ee109c513eff
makampos <makampos@DESKTOP-EDAK032>
2020-12-10 16:29:00 -0300

    feat: implementado métodos no CategoriasController para realizar operações no banco de dados

---
+++ APICatologo/Controllers/CategoriasController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APICatologo.Context;
+using APICatologo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatologo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriasController : Controller
+    {
+        private readonly AppDbContext _context;
+        public CategoriasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Retorna uma lista de categorias
+        /// </summary>
+        /// <returns>Categorias</returns>
+        [HttpGet]
+        public ActionResult<IEnumerable<Categoria>> Get()
+        {
+            return _context.Categorias.AsNoTracking().ToList();
+        }
+
+        /// <summary>
+        /// Retorna uma categoria pelo seu identificador
+        /// </summary>
+        /// <param name="id">Identificador</param>
+        /// <returns>Categoria</returns>
+        [HttpGet("{id}", Name = "ObterCategoria")]
+        public ActionResult<Categoria> Get(int id)
+        {
+            var categoria = _context.Categorias.AsNoTracking().FirstOrDefault(p => p.CategoriaId == id);
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+            return categoria;
+        }
+
+        /// <summary>
+        ///  Criar uma nova categoria
+        /// </summary>
+        /// <param name="categoria">Objeto categoria</param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Post([FromBody] Categoria categoria)
+        {
+            _context.Categorias.Add(categoria);
+            _context.SaveChanges();
+            return new CreatedAtRouteResult("ObterCategoria", new { id = categoria.CategoriaId }, categoria);
+        }
+
+        /// <summary>
+        /// Alterar uma categoria
+        /// </summary>
+        /// <param name="id">Identificador</param>
+        /// <param name="categoria">Objeto Categoria</param>
+        /// <returns></returns>
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Categoria categoria)
+        {
+            if (id != categoria.CategoriaId)
+            {
+                return BadRequest();
+            }
+            _context.Entry(categoria).State = EntityState.Modified;
+            _context.SaveChanges();
+            return Ok();
+        }
+
+        /// <summary>
+        /// Deletar uma categoria
+        /// </summary>
+        /// <param name="id">Identificador</param>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        public ActionResult<Categoria> Delete(int id)
+        {
+            var categoria = _context.Categorias.FirstOrDefault(p => p.CategoriaId == id);
+            if(categoria == null)
+            {
+                return NotFound();
+            }
+            _context.Categorias.Remove(categoria);
+            _context.SaveChanges();
+            return categoria;
+        }
+    }
+}
