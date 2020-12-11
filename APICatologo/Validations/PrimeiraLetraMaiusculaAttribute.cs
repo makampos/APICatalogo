bad42d8dc20725f9d58bdd84064facb0f73e306d
makampos <makampos@DESKTOP-EDAK032>
2020-12-11 18:31:29 -0300

    feat: Implementado a validação customizada utilizando DataAnnotations

---
+++ APICatologo/Validations/PrimeiraLetraMaiusculaAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICatologo.Validations
+{
+    /// <summary>
+    /// Validação customizada utilizando DataAnnotations
+    /// </summary>
+    public class PrimeiraLetraMaiusculaAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(Object value, ValidationContext validationContext)
+        {
+            if (value == null || string.IsNullOrEmpty(value.ToString()))
+            {
+                return ValidationResult.Success;
+            }
+
+            var primeiraLetra = value.ToString()[0].ToString();
+            if (primeiraLetra != primeiraLetra.ToUpper())
+            {
+                return new ValidationResult("A primeira letra do nome do produto deve ser maiúscula");
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
