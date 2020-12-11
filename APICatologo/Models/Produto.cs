2293ead7541cca7b6690773038152a9c1971a9c2
makampos <makampos@DESKTOP-EDAK032>
2020-12-11 18:29:15 -0300

    feat: Implementado validação customizada para o modelo Produto

--- APICatologo/Models/Produto.cs
+++ APICatologo/Models/Produto.cs
@@ -4,16 +4,18 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+using APICatologo.Validations;
 
 namespace APICatologo.Models
 {
     [Table("Produtos")]
-    public class Produto
+    public class Produto : IValidatableObject
     {
         [Key]
         public int ProdutoId { get; set; }
         [Required]
         [MaxLength(80)]
+        //[PrimeiraLetraMaiuscula]
         public string Nome { get; set; }
         [Required]
         [MaxLength(300)]
@@ -28,5 +30,40 @@ namespace APICatologo.Models
         // Propriedade de navegação
         public Categoria Categoria { get; set; }
         public int CategoriaId { get; set; }
-    }
+
+        /// <summary>
+        /// Implementa a interface IValidatableObject para costumizar uma validação no modelo
+        /// ! não permite reuso ao contrario da implementação da ValidationAttribute na pasta Validations.
+        /// </summary>
+        /// <param name="validationContext"></param>
+        /// <returns></returns>
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(this.Nome))
+            {
+                var primeiraLetra = this.Nome[0].ToString();
+                if (primeiraLetra != primeiraLetra.ToUpper())
+                {
+                    yield return new                 
+                        ValidationResult("A primeira letra do produto deve ser maiúscula",
+                        new[] { nameof(this.Nome) }
+
+                    );
+                }
+            }
+            if (this.Estoque <= 0)
+            {
+                yield return new
+                       ValidationResult("O estoque deve ser maior que 0",
+                       new[] { nameof(this.Estoque) }
+
+                   );
+            }
+
+         }
+    }     
+        
 }
+
+
