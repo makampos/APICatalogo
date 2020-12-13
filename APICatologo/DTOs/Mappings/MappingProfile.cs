9e087a12aa426ed7daca3f39ecf2dacaed879bcc
makampos <matheus.campos@edu.unifil.br>
2020-12-13 17:11:07 -0300

    feat: Configurado AutoMapper e registrado serviço

---
+++ APICatologo/DTOs/Mappings/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APICatologo.Models;
+using AutoMapper;
+
+namespace APICatologo.DTOs.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Produto, ProdutoDTO>().ReverseMap();
+            CreateMap<Categoria, CategoriaDTO>().ReverseMap();
+        }
+    }
+}
--- APICatologo/Startup.cs
+++ APICatologo/Startup.cs
@@ -1,7 +1,9 @@
 using APICatologo.Context;
+using APICatologo.DTOs.Mappings;
 using APICatologo.Extensions;
 using APICatologo.Repository;
 using APICatologo.Services;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -24,6 +26,14 @@ namespace APICatologo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var mappingConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new MappingProfile());
+            });
+
+            IMapper mapper = mappingConfig.CreateMapper();
+
+            services.AddSingleton(mapper);
             services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddDbContext<AppDbContext>(options => options.UseMySql
             (Configuration.GetConnectionString("DefaultConnection")));
