48c3f06ec80041f3461fe49b10386c916c6e239a
makampos <matheus.campos@edu.unifil.br>
2020-12-14 17:18:06 -0300

    feat: Implementação dos métodos registrar e logar usuário

---
+++ APICatologo/Controllers/AutorizaController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APICatologo.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatologo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutorizaController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        public AutorizaController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "AutorizaController :: Acessado em : " + DateTime.Now.ToLongDateString();
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult> RegisterUser([FromBody] UsuarioDTO model) {
+            var user = new IdentityUser
+            {
+                UserName = model.Email,
+                Email = model.Email,
+                EmailConfirmed = true
+
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if(!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
+            await _signInManager.SignInAsync(user, false);
+
+            return Ok();
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login([FromBody] UsuarioDTO userInfo)
+        {
+            var result = await _signInManager.PasswordSignInAsync(
+                userInfo.Email, userInfo.Password, isPersistent: false, lockoutOnFailure: false);
+            if (result.Succeeded)
+            {
+                return Ok();
+            } 
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Login inválido....");
+                return BadRequest(ModelState);
+            }
+        }
+    }
+}
